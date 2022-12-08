8ff95cb25bcdec024209b83cde6e5d511c3457ad
Teresa Pham <teresa.pham@gmail.com>
2022-12-08 10:10:59 -0800

    Add files via upload

--- Sprint4Project.py
+++ Sprint4Project.py
@@ -32,24 +32,52 @@ import plotly.express as px
 
 
 data =  pd.read_csv('vehicles_us.csv')
+data.info()
 
+
+# ## Data Cleanup - missing values, duplicates, change type
+# 1. Convert missing values for cylinders to the median of all non-missing cylinder values
+# 2. Convert missing values for odometer to the mean of all non-missing odometer values
+# 3. Convert missing 'is_4wd' (either 0 - no, or 1 - yes) to median non-missing is_4wd values
+# 4. Convert missing paint_color to 'unknown' string
+
+# # Having some issues with converting missing values in 'is_4wd' to boolean - see errors below
 
 # In[3]:
 
 
-# convert model_year, cylinders, odometer, and is_4wd from floating to integers
+# Missing Values: convert model_year, cylinders, odometer, and is_4wd to more suitable values
 
-data['cylinders'] = data['cylinders'].fillna(0).astype(int)
-data['odometer'] = data['odometer'].fillna(0).astype(int)
-data['is_4wd'] = data['is_4wd'].fillna(0).astype(int)
+data['cylinders'] = data['cylinders'].fillna(data['cylinders'].median())
+data['odometer'] = data['odometer'].fillna(data['odometer'].mean())
+data['is_4wd'] = data['is_4wd'].fillna(data['is_4wd'].a.bool())
+data['model_year'] = data['model_year'].fillna(data['model_year'].median())
+data['paint_color'] = data['paint_color'].fillna('unknown')
 
 
-# convert date_posted to date_time
+# <b> There was a reviewer comment to adjust model_year, but I don't really use it in my graphs, so I decided not to modify it. </b>
 
+# In[ ]:
+
+
+# Format Change: convert date_posted to date_time
 data['date_posted'] = pd.to_datetime(data['date_posted'], format = '%Y-%m-%d')
 
 
-# In[4]:
+# In[ ]:
+
+
+# Remove Duplicates:
+data = data.drop_duplicates(subset = None, keep = 'first')
+
+
+# In[ ]:
+
+
+data.info()
+
+
+# In[ ]:
 
 
 # creating title for page
@@ -57,7 +85,7 @@ data['date_posted'] = pd.to_datetime(data['date_posted'], format = '%Y-%m-%d')
 st.header("Pricing Analysis")
 
 
-# In[5]:
+# In[ ]:
 
 
 # create options for histogram elements by creating selectbox
@@ -65,7 +93,7 @@ list_for_hist = ['is_4wd','model','condition']
 choice_for_hist = st.selectbox('Choose Options', list_for_hist)
 
 # create histogram using plotly-express
-fig1 = px.histogram(data, x = 'price', color = choice_for_hist)
+fig1 = px.histogram(data, x ="price", color = choice_for_hist)
 fig1.update_layout(
 title = '<b>Price Analysis -- {}</b>'.format(choice_for_hist))
 
@@ -73,7 +101,7 @@ title = '<b>Price Analysis -- {}</b>'.format(choice_for_hist))
 st.plotly_chart(fig1)
 
 
-# In[7]:
+# In[ ]:
 
 
 # create distribution for scatterplot
@@ -85,6 +113,12 @@ fig2 = px.scatter(data,x = choice_for_scatter, y = 'price')
 fig2.update_layout(
 title = '<b>Price based on {}</b>'.format(choice_for_scatter))
 st.plotly_chart(fig2)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
